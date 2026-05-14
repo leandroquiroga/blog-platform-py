d60b4e93722e3a06cc094e40d8a23e22eddb10cb
leandroquiroga <leandroquiroga9514@gmail.com>
2026-05-14 19:02:17 -0300

    implement PostModel

---
+++ app/models/blog_models.py
@@ -0,0 +1,13 @@
+from pydantic import Field
+from beanie import  Document
+from datetime import datetime
+
+class PostModel(Document):
+    """ Model for a blog post. """
+    title: str = Field(..., min_length=3, max_length=50)
+    content: str = Field(..., min_length=10, max_length=500)
+    category: str = Field(..., min_length=3, max_length=20)
+    tags: list[str] = Field(default_factory=list)
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now) 
+    
