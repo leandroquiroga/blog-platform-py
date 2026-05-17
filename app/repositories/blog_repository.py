5a4f054247611c4293cc4a031629b3a7e4340237
leequiro <leandroquiroga9514@gmail.com>
2026-05-17 17:59:21 -0300

    implement repository BlogRepository

---
+++ app/repositories/blog_repository.py
@@ -0,0 +1,49 @@
+from app.schemas.blog_schema import PostCreateSchema, PostResponseSchema
+from app.models.blog_models import BlogModel
+
+class BlogRepository:
+    """ Repository for managing blog posts. """
+    
+    async def create_post(self, post_data: PostCreateSchema) -> BlogModel:
+        """ Create a new blog post. """
+        post = BlogModel(**post_data.dict())
+        await post.insert()
+        return post
+      
+    async def get_post(self, post_id: str) -> BlogModel | None:
+        """ Get a blog post by it's ID. """
+        post =  await BlogModel.get(post_id)
+        
+        if not post: 
+            return None
+        
+        return post
+    
+    async def get_all_posts(self) -> list[BlogModel]:
+        """ Get all blog posts. """
+        return await BlogModel.find_all().to_list()
+    
+    async def update_post(self, post_id: str, post_data: PostCreateSchema) -> BlogModel | None:
+        """ Update a blog post by it's ID. """
+        post = await BlogModel.get(post_id)
+        
+        if not post: 
+            return None
+
+        post.title = post_data.title
+        post.content = post_data.content
+        post.category = post_data.category
+        post.tags = post_data.tags
+        
+        await post.save()
+        return post
+    
+    async def delete_post(self, post_id: str) -> bool:
+        """ Delete a blog post by it's ID. """
+        post = await BlogModel.get(post_id)
+        
+        if not post: 
+            return False
+        
+        await post.delete()
+        return True
