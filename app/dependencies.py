0c4dc0ad9f40e9fede90bd06c0e82fb37ad2ae18
leandroquiroga <leandroquiroga9514@gmail.com>
2026-05-24 20:28:17 -0300

    implement get_current_user and get_auth_user authentication dependencies

--- app/dependencies.py
+++ app/dependencies.py
@@ -15,3 +15,33 @@ def get_cache_utils():
     from app.utils.cache_utils import CacheUtils
     
     return CacheUtils(redis_client)
+
+from app.services.auth_services import AuthService
+from app.repositories.user_repository import UserRepository
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer,  HTTPAuthorizationCredentials
+from app.models.user_models import UserModel
+
+# Security dependency for authentication
+security = HTTPBearer(auto_error=False)
+
+
+# Dependency to get the AuthService instance
+def get_auth_service():
+    """ Dependency to get the AuthService instance. """
+    
+    repository =  UserRepository()
+    service = AuthService(repository)
+    return service
+
+
+
+# Dependency to get the current authenticated user
+async def get_current_user(   
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+    service: AuthService = Depends(get_auth_service),) -> UserModel:
+    """ Dependency to get the current authenticated user """
+    if credentials is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
+    return await service.get_current_user(credentials.credentials)
+    
