ef3ff02109eb3b16eb9892e07551e49ff435a18c
leandroquiroga <leandroquiroga9514@gmail.com>
2026-05-25 12:48:58 -0300

    update expiration logic in create_access_token

--- app/utils/secutiry_utils.py
+++ app/utils/secutiry_utils.py
@@ -19,13 +19,13 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 def create_access_token(data: dict ) -> str:
     """ Creates a JWT access token with the given data and expiration time """
     to_encode = data.copy()
-    expire = datetime.now() + timedelta(minutes=settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
+    expire = datetime.now() + timedelta(minutes=int(settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire.timestamp()})
     return jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
 
 def decode_access_token(token: str) -> dict | None:
     """ Decodes a JWT access token and returns the payload data """
-    try:
+    try: 
         return jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
     except JWTError:
         return None
