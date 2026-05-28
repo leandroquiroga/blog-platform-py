dedcb7b509ae2df4c370095a303f7c7d4b829985
leandroquiroga <leandroquiroga9514@gmail.com>
2026-05-28 19:48:51 -0300

    add authenticated /me endpoint to retrieve current user and their posts

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from app.services.auth_services import AuthService
+from app.services.blog_service import BlogService
 from app.schemas.user_schema import UserCreateSchema, UserResponseSchema, LoginSchema, TokenSchema
-from app.dependencies import get_auth_service
+from app.schemas.blog_schema import UserWithBlogsResponseSchema
+from app.dependencies import get_auth_service, get_blog_service
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
@@ -17,9 +19,19 @@ async def register_user(user_data: UserCreateSchema, service: AuthService = Depe
 async def login_user(login_data: LoginSchema, service: AuthService = Depends(get_auth_service)):
     return await service.login_user(login_data)
 
-@router.get("/me", response_model=UserResponseSchema)
-async def get_me(credentials: HTTPAuthorizationCredentials = Depends(security), service: AuthService = Depends(get_auth_service)):
+@router.get("/me", response_model=UserWithBlogsResponseSchema)
+async def get_me(
+        credentials: HTTPAuthorizationCredentials = Depends(security), 
+        service: AuthService = Depends(get_auth_service),
+        blogs: BlogService = Depends(get_blog_service)
+    ):
     if credentials is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
-    return await service.get_current_user(credentials.credentials)
+    user =  await service.get_current_user(credentials.credentials)
+    user_blogs = await blogs.get_my_posts(str(user.id))
+    user_validated = UserResponseSchema.model_validate(user)
     
+    return UserWithBlogsResponseSchema(
+        **user_validated.model_dump(),
+        blogs=user_blogs
+    )
