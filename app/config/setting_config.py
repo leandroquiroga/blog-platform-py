60ab0d23096d69d3af0d70813b8bd992f439fc7b
leandroquiroga <leandroquiroga9514@gmail.com>
2026-05-31 19:38:56 -0300

    add rate_limit count and windo property to configuration

--- app/config/setting_config.py
+++ app/config/setting_config.py
@@ -1,8 +1,10 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
-    """ Configuration settings for the application. """
+    """Configuration settings for the application."""
+
     URL_MONGO_DB: str = Field(validation_alias="URL_MONGO_DB")
     DATABASE_NAME: str = Field(validation_alias="DATABASE_NAME")
     REDIS_HOST: str = Field(validation_alias="REDIS_HOST")
@@ -10,18 +12,29 @@ class Settings(BaseSettings):
     REDIS_DB: int = Field(validation_alias="REDIS_DB")
     JWT_SECRET_KEY: str = Field(validation_alias="JWT_SECRET_KEY")
     JWT_ALGORITHM: str = Field(validation_alias="JWT_ALGORITHM")
-    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(validation_alias="JWT_ACCESS_TOKEN_EXPIRE_MINUTES")
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(
+        validation_alias="JWT_ACCESS_TOKEN_EXPIRE_MINUTES"
+    )
     
     RATE_LIMIT: str = Field(
         default="10/minute",
         validation_alias="RATE_LIMIT",
     )
-    
+
+    @property
+    def rate_limit_count(self) -> int:
+        """Extract the count from the RATE_LIMIT string."""
+        return int(self.RATE_LIMIT.split("/")[0])
+
+    @property
+    def rate_limit_window(self) -> int:
+        """Extract the time window in seconds from the RATE_LIMIT string."""
+        period = self.RATE_LIMIT.split("/")[1]
+        return {"minute": 60, "hour": 3600, "day": 86400}[period]
+
     model_config = SettingsConfigDict(
-        env_file=".env",
-        env_file_encoding="utf-8",
-        case_sensitive=True
+        env_file=".env", env_file_encoding="utf-8", case_sensitive=True
     )
 
 
-settings = Settings() # pyright: ignore[reportCallIssue]
+settings = Settings()  # pyright: ignore[reportCallIssue]
